210e248735799790c6d76fd8357cfe4f7a317779
r2hu1 <87074472+r2hu1@users.noreply.github.com>
2024-01-04 14:09:39 +0530

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -2,7 +2,7 @@ import { authMiddleware } from "@clerk/nextjs";
 
 
 export default authMiddleware({
-    publicRoutes:["/"]
+    publicRoutes: req => !req.url.includes('/dashboard')
 }); 
 export const config = {
   matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
