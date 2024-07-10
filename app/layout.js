c989e4d0bb8fa7439d77e8d18eff25381f307f1e
Rahul Rajput <newasram86@gmail.com>
2024-07-10 13:26:58 +0000

    removed glowing bg

--- app/layout.js
+++ app/layout.js
@@ -42,7 +42,6 @@ export default function RootLayout({ children }) {
               zIndex={1600}
               showAtBottom={false}
             />
-            <div className="absolute inset-0 -z-10 h-[600px] w-full bg-background bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:40px_40px]"><div className="absolute left-0 right-0 top-0 -z-10 m-auto h-[310px] w-[310px] rounded-full bg-primary opacity-10 dark:opacity-25 blur-[100px]"></div></div>
             {children}
             <Toaster />
           </ThemeProvider>
