48fcfd0ac34c3b077c88938661aef14c2f9d7f58
Rahul Rajput <newasram86@gmail.com>
2024-07-10 13:26:49 +0000

    used more-shadcn-buttons config

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,6 +17,9 @@ module.exports = {
       },
     },
     extend: {
+      backgroundImage: {
+        "skeleton": "linear-gradient(270deg, hsl(var(--secondary)), hsl(var(--secondary) / 80%), hsl(var(--secondary) / 70%), hsl(var(--secondary) / 50%))",
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
@@ -66,10 +69,25 @@ module.exports = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: "0" },
         },
+        "pulseGlow": {
+          from: { boxShadow: "0 0 0 0 hsl(var(--primary) / 25%)" },
+          to: { boxShadow: "0 0 0 10px rgba(255, 255, 255, 0)" },
+        },
+        "spinner": {
+          from: { transform: "rotate(0deg)" },
+          to: { transform: "rotate(360deg)" },
+        },
+        "skeleton": {
+          '0%, 100%': { backgroundPosition: '0% 0%' },
+          '50%': { backgroundPosition: '100% 100%' },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        "pulseGlow": "pulseGlow 1.75s infinite cubic-bezier(0.66, 0, 0, 1)",
+        "spinner": "spinner 1.2s linear infinite",
+        "skeleton": 'skeleton 8s ease-in-out infinite',
       },
     },
   },
