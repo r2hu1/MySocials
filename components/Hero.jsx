6a75ae73120aa7a01d767deaaa4569dd1abaae9b
Rahul Rajput <newasram86@gmail.com>
2024-07-10 14:57:19 +0000

    sc to out

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -22,7 +22,7 @@ export default async function Hero() {
             </div>
             <div className="flex items-center sm:justify-center gap-3 mt-7">
                 <Button asChild variant="pulseBtn"><Link href={!user ? "/sign-in" : "/dashboard/profile"}>Get Started</Link></Button>
-                <Button variant="secondary" asChild><Link href="/r2hu1" className="flex items-center gap-1">Demo <ArrowRight className="h-4 w-4 -rotate-45" /></Link></Button>
+                <Button variant="outline" asChild><Link href="/r2hu1" className="flex items-center gap-1">Demo <ArrowRight className="h-4 w-4 -rotate-45" /></Link></Button>
             </div>
             <div className="mt-20 bg-secondary p-1 rounded-lg w-fit mx-auto">
                 <DemoImage />
