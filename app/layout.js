e6de18bac737d554b0edd408f83025b7f5e57132
Rahul Rajput <newasram86@gmail.com>
2024-07-11 15:28:41 +0000

    lig

--- app/layout.js
+++ app/layout.js
@@ -38,7 +38,7 @@ export default function RootLayout({ children }) {
             />
           <ThemeProvider
             attribute="class"
-            defaultTheme="system"
+            defaultTheme="light"
             enableSystem
             disableTransitionOnChange
           >
