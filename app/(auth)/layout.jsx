9018b9799f4aa1b74749cbf6e05f9ef9f94693da
Rahul Rajput <newasram86@gmail.com>
2024-07-11 23:51:27 +0530

    Update layout.jsx

--- app/(auth)/layout.jsx
+++ app/(auth)/layout.jsx
@@ -2,7 +2,7 @@ import { Skeleton } from "@/components/ui/skeleton";
 import { ClerkLoading } from "@clerk/nextjs";
 export default function RootLayout({ children }) {
     return (
-        <main className="fixed top-0 left-0 right-0 h-full w-full flex justify-center items-center">
+        <main className="fixed top-0 left-0 right-0 h-full w-full flex justify-center items-center px-6">
             <ClerkLoading>
                 <Skeleton className="h-[264px] w-[500px] max-w-[300px]" />
             </ClerkLoading>
