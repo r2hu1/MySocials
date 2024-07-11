b124ebd8b07d71a4cf71f20a04454cbddeca1ec1
r2hu1 <newasram86@gmail.com>
2024-07-11 23:25:57 +0530

    []

--- app/api/create/route.js
+++ app/api/create/route.js
@@ -1,23 +1,30 @@
 import User from "@/models/user";
 import { currentUser } from "@clerk/nextjs";
-import { colors } from "@clerk/themes/dist/clerk-js/src/ui/foundations/colors";
 import mongoose from "mongoose";
 import { NextResponse } from "next/server";
 
 export async function POST(request) {
-    let { emailAddresses, imageUrl } = await currentUser();
-    let email = emailAddresses[0].emailAddress;
+    await mongoose.connect(process.env.NEXT_MONGO_URI);
+    
+    const { emailAddresses, imageUrl } = await currentUser();
+    const email = emailAddresses[0].emailAddress;
+    console.log(email);
+
     const { username, bio, youtube, insta, face, github, name } = await request.json();
+
     try {
-        await mongoose.connect(process.env.NEXT_MONGO_URI);
-        const findCurrentUser = await User.findOne({ email });
+        const findCurrentUser = await User.findOne({ email: email });
+
+        const getSimilarUsername = await User.findOne({ username: username });
+        if (getSimilarUsername && getSimilarUsername.email !== email) {
+            console.log(getSimilarUsername.email);
+            return NextResponse.json({ error: "Username is already in use" }, { status: 500 });
+        }
+
         if (findCurrentUser) {
-            const getSimilarUsername = await User.findOne({ username: username });
-            if (getSimilarUsername.email != email) {
-                return NextResponse.json({ error: "Username is already in use" }, { status: 500 });
-            }
-            else {
-                const findAndUpdateCurrentUser = await User.findOneAndUpdate({ email }, {
+            await User.findOneAndUpdate(
+                { email: email },
+                {
                     name: name,
                     username: username,
                     bio: bio,
@@ -26,32 +33,23 @@ export async function POST(request) {
                     facebook: face,
                     github: github,
                     image: imageUrl
-                });
-                return NextResponse.json({ "message": "success", name, username, bio, youtube, insta, face, github }, { status: 200 });
-            }
+                }
+            );
+            return NextResponse.json({ message: "success", name, username, bio, youtube, insta, face, github }, { status: 200 });
+        } else {
+            await User.create({
+                name: name,
+                username: username,
+                bio: bio,
+                youtube: youtube,
+                instagram: insta,
+                facebook: face,
+                github: github,
+                image: imageUrl
+            });
+            return NextResponse.json({ message: "success", name, username, bio, youtube, insta, face, github }, { status: 200 });
         }
-        else {
-            const getSimilarUsername = await User.findOne({ username: username });
-            if (getSimilarUsername.email != email) {
-                return NextResponse.json({ error: "Username is already in use" }, { status: 500 });
-            }
-            else {
-                const makeNewUser = await User.create({
-                    name: name,
-                    username: username,
-                    email: email,
-                    bio: bio,
-                    youtube: youtube,
-                    instagram: insta,
-                    facebook: face,
-                    github: github,
-                    image: imageUrl
-                });
-                return NextResponse.json({ "message": "success", name, username, bio, youtube, insta, face, github }, { status: 200 });
-            }
-        }
-    }
-    catch (error) {
+    } catch (error) {
         return NextResponse.json({ error: error.message }, { status: 500 });
     }
-};
+}
